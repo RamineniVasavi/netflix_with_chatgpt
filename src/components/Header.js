5149899b6989f5db855075e4b9fb248f94a81526
RamineniVasavi <vasaviramineni9@gmail.com>
2024-10-29 19:18:09 +0530

    Icon width

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import logo from "../utils/Netflix_Logo_PMS.png"
 const Header = () => {
   return (
-    <div className='absolute mx-10 py-2 w-96 bg-gradient-to-tr from-black'>
+    <div className='absolute mx-10 py-2 w-60 bg-gradient-to-tr from-black'>
       <img  src={logo} alt="logo"></img>
     </div>
   )
